e0fe3dbf35a4a5c7611da327193db073dbce7937
kumanxuan <775121173@qq.com>
2017-09-15 17:23:35 +0800

    测试了__str__,私有变量，私有方法，还有定义方法的时候，后面带入的参数不一样是self，可以是其他，但是一般是self，可以方便对象对用成员或者方法

--- moduleTest.py
+++ moduleTest.py
@@ -4,6 +4,53 @@ print("这个是测试模块导入的")
 
 #pythonMysql.testMysql()
 
-testMysql()
+
+class VarX:
+
+
+ def __init__(self):
+   self.name=''
+   self.__nickName = 'beetle'
+
+ def __printX(self):
+    name = 'kumanxuan6666666666'
+    print(self.__nickName)
+    print(name)
+
+ def test1Print(self):
+     __string = 'lizhixuan'
+     print(__string)
+
+ def testPrint(self):
+    num = 24
+    self.name = 'kumanxuan'
+    self.__printX()
+    return self.name
+
+ def test2Print(x):
+    name = 'kumanjin'
+    return name
+
+ def __str__(self):
+    return "你就是最棒棒了我！！"
+
+#testMysql()
 
 print("我现在正在用window10的系统，pycharm工具来编写代码的！！")
+
+test = VarX()
+val = test.testPrint()
+print(val)
+
+print(test.test2Print())
+
+print(test)
+
+print(test.name)
+
+
+
+
+
+
+
