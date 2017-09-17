46cf0658d49c2c2742b8e7f76eb73b34784c208e
kuman-lenovo <775121173@qq.com>
2017-09-17 18:44:59 +0800

    add a package

---
+++ xuanPrint/package2/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['printWorld']
+
+from . import printWorld
+
+print("喵喵喵，不管了，我要说话!!")
---
+++ xuanPrint/package2/printWorld.py
@@ -0,0 +1,6 @@
+class dog:
+    def __init__(self):
+        print("I am a little dog")
+
+def cat():
+    print("I am a cat,miao miao miao!! ")
