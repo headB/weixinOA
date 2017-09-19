3ce0a31853b448bf55a104e86f19ad71e208d1b5
kumanxuan <775121173@qq.com>
2017-09-19 09:58:43 +0800

    测试一个普通的方法设置值，普通私有方法，等下尝试property。

---
+++ property.py
@@ -0,0 +1,22 @@
+print("测试python里面的property用法")
+
+class score:
+    def __init__(self):
+        self.__num = 0
+        print("默认分数是0分")
+
+    def setScore(self,score):
+
+        #做了一个简单的判断，只能接收int
+        if not type(score)==int:
+            print("error value")
+            exit()
+        self.__num = score
+        print("success to set score")
+
+    def getScore(self):
+        return self.__num
+
+tom = score()
+tom.setScore(60)
+print(tom.getScore())
