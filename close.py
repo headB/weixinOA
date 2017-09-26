8e1984765430a7af7fe591ad48007b022ac8dc9c
kumanxuan <775121173@qq.com>
2017-09-26 10:10:44 +0800

    测试python的列表生成式，生成器。

--- close.py
+++ close.py
@@ -9,9 +9,30 @@ def test(num):
 
     return test1
 
-ret = test(100)
-print('----3----')
+#ret = test(100)
+#print('----3----')
 
-ret(200)
+#ret(200)
 
 ##等于300
+
+
+
+##测试迭代器??测试生成器??测试快速循环。
+#我的第一个列表生成式。
+c= [ x*x+1 for x in range(1,8)]
+#print(c)
+print(type(c))
+
+d = ([ x*x+1 for x in range(1,8)])
+print(d)
+print(type(d))
+
+##我的第一个迭代器。
+f = ( x*x+1 for x in range(1,8))
+print(type(f))
+
+##上面的执行结果。<class 'list'>
+#<class 'list'>
+#<class 'generator'>
+
