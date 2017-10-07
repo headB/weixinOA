307ccbf57b3c554b8b58444cad37b5af2d269209
kumanxuan <775121173@qq.com>
2017-10-07 17:04:43 +0800

    测试兼容多平台的Process.

--- network.py
+++ network.py
@@ -2,5 +2,7 @@ from  socket import *
 
 myFirstSocket = socket(AF_INET,SOCK_DGRAM)
 
-myFirstSocket.sendto("",('127.0.0.1:2425',2425))
+#myFirstSocket.sendto("1:12525:kumanxuan:kumanxuan:32:my name is kumanxuan",("127.0.0.1",2425))
+#修正，直接发送字符串的话，需要在前面加一个B。
+myFirstSocket.sendto(b"1:12525:kumanxuan:kumanxuan:32:my name is kumanxuan",('127.0.0.1',2425))
 
