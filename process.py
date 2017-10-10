a1867f307a6701c8d5b52954ece6feda4e8781d5
kumanxuan <775121173@qq.com>
2017-10-10 14:55:47 +0800

    测试process的join。

--- process.py
+++ process.py
@@ -3,7 +3,7 @@ import time
 print("测试兼容多平台的多进程")
 
 def test():
-    while True:
+    for i in range(5):
         print('--test--')
         time.sleep(1)
 
@@ -12,6 +12,10 @@ test1 = Process(target=test)
 
 test1.start()
 
-while True:
-    print("hello!!")
-    time.sleep(1)
+##调用对象的方法，就是，拥塞，等待子进程执行完之后我再继续执行
+##意义何在？？？
+test1.join()
+
+#while True:
+#    print("hello!!")
+#    time.sleep(1)
