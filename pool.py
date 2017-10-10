70c78f7aec08b315134f7d997d40007ccfbaba27
kuman-lenovo <775121173@qq.com>
2017-10-10 21:55:26 +0800

    验证创建了3个进程池，然后分配10个任务，然后3个进程池去同时并发轮询做任务。

--- pool.py
+++ pool.py
@@ -4,16 +4,16 @@ import random
 import time
 
 ##定义一个普通函数
-def worker():
+def worker(num):
     #那下面这里是这样定义咯，随机返回一个数来循环。
     for i in range(10):
-        print("-=--pid =%d "%(i))
+        print("-=--pid =%d and num is %s"%(os.getpid(),num))
         time.sleep(1)
 #
 pool = Pool(3)
 
 for i in range(10):
-    pool.apply_async(worker)
+    pool.apply_async(worker,(i,))
 
 
 ##close是什么???
