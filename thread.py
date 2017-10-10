f40b681c7c2645ba9e18ba5e5ab0d4adcb1fabab
root <root@apple47.(none)>
2017-10-10 14:16:21 +0800

    利用fork创建的进程，的确是同时进行，而且各自运行，谁都不管谁

--- thread.py
+++ thread.py
@@ -10,11 +10,19 @@ ret = os.fork()
 #print(os.getpid())
 
 if ret ==0:
-   while True:
+   #while range(1,4):
+#就是假设父进程已经结束了，子进程还在执行程序，fork函数创建出来的
+#父进程就不等了，各位发展。
      print('xx,我已经是子进程来的')
-     time.sleep(1)
+     print('xx,我已经是子进程来的')
+     print('xx,我已经是子进程来的')
+     print('xx,我已经是子进程来的')
+     print('xx,我已经是子进程来的')
+     time.sleep(4)
 
 else:
-  while True:
+  #while range(1,3):
    print("yy，我已经是父进程控制的，应该是")
-   time.sleep(2)
+   time.sleep(3)
+
+print("the end")
