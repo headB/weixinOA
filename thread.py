9f0dad95cdd2ebce877f853ce0bdb293e9f79c6b
root <root@apple47.(none)>
2017-10-10 11:39:08 +0800

    新建了一个进程，修改了，然后现在在linux端提交。

--- thread.py
+++ thread.py
@@ -4,5 +4,17 @@ import os
 print("测试进程")
 
 ret = os.fork()
+#print(ret)
+##ret里面的值，应该是返回新建的子进程的pid来的。
+#print("+++")
+#print(os.getpid())
 
-print(ret)
+if ret ==0:
+   while True:
+     print('xx,我已经是子进程来的')
+     time.sleep(1)
+
+else:
+  while True:
+   print("yy，我已经是父进程控制的，应该是")
+   time.sleep(2)
