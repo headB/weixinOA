2e7c00195f96430862e63bac44312cbdcb6683fa
kumanxuan-centos47 <775121173@qq.com>
2017-10-11 15:51:05 +0800

    修改好，忘记用print打印出变量，以后自己不会用Queue

--- pool.py
+++ pool.py
@@ -1,4 +1,4 @@
-from multiprocessing import Pool,Queue
+from multiprocessing import Pool,Manager
 import os
 import random
 import time
@@ -7,10 +7,7 @@ import time
 ##假设在这里增加函数，应该不会被重复的，因为按照fork的方法，写在前面的代码应该不会复制过去执行。
 ##针对上面的言论，这个全局变量的函数，linux只会执行一次，所以，注意小心一点了。
 ##window和linux运行结果不一样，尽量以linux为准。
-def printTest2():
-    print("can program runing me ???if can, how many times??")
 
-#printTest2()
 
 ##经过测试，上面如果调用了函数的话，在每一个进程都会被执行一次，哎，晕。。。。
 ##那意思就是，函数可以写好，但是不要随便调用，要调用的话，用下面那个pool.apply_async调用。
@@ -35,24 +32,27 @@ if __name__ == "__main__":
     ##close是什么???
     pool.close()
     pool.join()
-    printTest2()
-    print("this is for a test!!")
+
+    q=Manager().Queue()
+    q.put("kumanxuan")
+    q.put("lizhixuan")
+    print(q.empty())
+    print(q.qsize())
+    print(q.get(True))
+    print(q.get(True))
 
 #print(q.get(True))
 
 ##测试了，然后发现，是window的问题，在linux上是没有问题的。
 
+#q=Queue()
+#q.put("kumanxuan")
+#q.put("lizhixuan")
 
-def printTest():
-    print("this is print by function printTest!!")
 
-#printTest()
 
-print(__name__)
-#print("kumanxuan!!")
 #q = Queue()
 #q.put('xx')
 #q.put('yy')
 #q.get()
 #q.get()
-
