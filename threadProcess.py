d50fde0e16918466a45383adf692a555afdc6215
kumanxuan <775121173@qq.com>
2017-10-12 15:48:21 +0800

    利用派生threading的子类，创建多线程运行。

--- threadProcess.py
+++ threadProcess.py
@@ -3,9 +3,24 @@ import time,threading
 def printTest():
     print("我要并发打印！！")
 
+class newThread(threading.Thread):
+    def run(self):
+        for i in  range(4):
+            time.sleep(1)
+            print("I'm "+self.name+str(i))
 
-if __name__=="__main__":
-    for x in range(5):
-        t = threading.Thread(target=printTest)
+
+#if __name__=="__main__":
+#    for x in range(5):
+#        t = threading.Thread(target=printTest)
+#        t.start()
+
+#创建thread的子类来执行运行多线程、
+if __name__ == "__main__":
+        t=newThread()
+        #t.setDaemon(False)默认不用设置
         t.start()
-
+        t1=newThread()
+        #t1.setDaemon(False)
+        t1.start()
+        print('hello world!!')
