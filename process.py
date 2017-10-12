b7eeb16e52178e0f32535e19b122ebf174cc6c2d
kumanxuan <775121173@qq.com>
2017-10-12 16:54:52 +0800

    对比线程和进程操作全局变量的对比，准备在linux测试结果，在window上符合结果。

--- process.py
+++ process.py
@@ -1,33 +1,24 @@
 from multiprocessing import Process
 import time
-print("测试兼容多平台的多进程")
+
+y='kumanxuan'
 
 def test():
-    for i in range(5):
+    for i in range(2):
+        global y
+        y+="cc1"
+        print(y)
         print('--test--')
         time.sleep(1)
 
-test1 = Process(target=test)
-#Process是大写的，不然变量冲突吧???
 
-test1.start()
+if __name__ == "__main__":
+    for x in range(2):
+        test1 = Process(target=test)
+        test1.start()
+    time.sleep(4)
+    print("hello!!")
+    print(y)
 
-##调用对象的方法，就是，拥塞，等待子进程执行完之后我再继续执行
-##意义何在？？？还是有意思的，可能添加超时，超时之后，父进程继续执行程序
 
-#test1.join()
-
-#然后到主进程执行
-#print("终于等到你！！！")
-
-##上面的执行过程就是，等到海枯石烂了，我都继续等你。
-
-##然后现在开始，我只等你3秒，然后我就走了。
-
-test1.join(3)
-
-print("不知道有没有等到了，如果等到你过来了，可能我们已经在一起，不然我就自己一个人走了。")
-
-##我是意思，请向我看齐
-##但是还是说明了一个问题，就是，父进程还是会执行程序，但等到最后，父进程还是会等子进程。
 
--- threadProcess.py
+++ threadProcess.py
@@ -1,26 +1,40 @@
 import time,threading
 
 def printTest():
+    global y
+    time.sleep(1)
+    y=y+'ccc1'
+    print(y)
     print("我要并发打印！！")
+
+
 
 class newThread(threading.Thread):
     def run(self):
         for i in  range(4):
+            global y1
+            y1=y1+'ddd1'
             time.sleep(1)
             print("I'm "+self.name+str(i))
 
+#定义全局变量
+y='kumanxuan1'
+y1='kumanxuan2'
 
-#if __name__=="__main__":
-#    for x in range(5):
-#        t = threading.Thread(target=printTest)
-#        t.start()
+if __name__=="__main__":
+    for x in range(5):
+        t = threading.Thread(target=printTest)
+        t.start()
+        #t.join()
+    time.sleep(3)
+    #print(y)
 
 #创建thread的子类来执行运行多线程、
+
 if __name__ == "__main__":
         t=newThread()
-        #t.setDaemon(False)默认不用设置
         t.start()
         t1=newThread()
-        #t1.setDaemon(False)
         t1.start()
-        print('hello world!!')
+        #print(y1)
+        #print('hello world!!')
