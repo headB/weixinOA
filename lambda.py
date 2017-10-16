08a84901164c4a97bebd1bbd73612bd0cfa51d6a
kumanxuan <775121173@qq.com>
2017-10-16 14:50:56 +0800

    测试了return原理，和php差不多，return后面就不要加任何的代码了，还有就是，python里面也真的是一切皆为对象。测试了闭包的原理。一点点。

--- lambda.py
+++ lambda.py
@@ -23,7 +23,7 @@ def demo(func):
 
 funcX = demo(funcX)
 
-print(funcX.__name__)
+#print(funcX.__name__)
 
 funcX()
 
@@ -33,3 +33,27 @@ def printMyName():
 
 
 printMyName()
+
+def testReturn():
+    def test2():
+        def tt():
+            print('cccc')
+        str="I am kumanxuan2222222222!!"
+        #return tt
+        #差点忘记了，就是如果这里使用了return的话，后面的代码就不执行的啦。！！
+        #哎呀，没记性啊。~！
+        tt()
+    return test2
+
+
+strs = testReturn()
+
+strs()
+
+##OK，顺便验证了，真的是，在python里面，也是一切皆为对象。
+print(testReturn)
+
+#print(testReturn())
+
+#print(testReturn())
+
