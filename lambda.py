58805be5461cf9f3c8667e7548e51f395db50ec4
kumanxuan <775121173@qq.com>
2017-10-16 13:19:12 +0800

    测试Python的修饰器，两种方法。

--- lambda.py
+++ lambda.py
@@ -4,4 +4,32 @@ def funcT(x,y,opt):
     print(y)
     print(opt(x,y))
 
+def funcX():
+    print("test the @")
+
 funcT(4,5,lambda x,y:x+y)
+
+print(funcT.__name__)
+
+def demo(func):
+    def wrapper():
+        func()
+        ##这里是可以添加新功能的！！
+        def testFunction():
+            print("I am a new function!!")
+        testFunction()
+
+    return wrapper
+
+funcX = demo(funcX)
+
+print(funcX.__name__)
+
+funcX()
+
+@demo
+def printMyName():
+    print("I am kumanxuan!!")
+
+
+printMyName()
