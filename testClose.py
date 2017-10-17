524716d1b01a2e64cf76bd6f6e239b1d569dece8
kumanxuan <775121173@qq.com>
2017-10-17 17:57:08 +0800

    测试闭包和，重点重点，测试python的装饰器。这个很重要。

---
+++ testClose.py
@@ -0,0 +1,21 @@
+from time import ctime, sleep
+
+def timefun(func):
+    def wrappedfunc():
+        print("%s called at %s"%(func.__name__, ctime()))
+        return func()
+    return wrappedfunc
+
+@timefun
+def foo():
+    print("I am foo")
+
+@timefun
+def getInfo():
+    return '----hahah---'
+
+foo()
+foo()
+
+
+print(getInfo())
