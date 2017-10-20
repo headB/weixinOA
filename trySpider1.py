303d22401fb7463e63a4b061c212db9772717467
kumanxuan <775121173@qq.com>
2017-10-20 15:45:48 +0800

    自己手动写一个模块的导入，熟练一下import，然后准备去试试from XXX import XX

--- trySpider1.py
+++ trySpider1.py
@@ -1,11 +1,20 @@
 from multiprocessing import Pool
 import urllib.request
+import xuanPrint.package2
+import xuanPrint
+
 ##定义进程池
 ##定义好数量
 pa = Pool(3)
 
+xuanPrint.sendMsg.sendMsg()
+newClass = xuanPrint.package2.printWorld.dog()
+
+newClass.testMod()
+
 def pachong():
-    res = urllib.request.urlopen("www.baidu.com")
+    pass
+    #res = urllib.request.urlopen("www.baidu.com")
 
 ##程序必写的这句话
 
--- xuanPrint/package2/printWorld.py
+++ xuanPrint/package2/printWorld.py
@@ -2,5 +2,9 @@ class dog:
     def __init__(self):
         print("I am a little dog")
 
+    def testMod(self):
+        print("I am belong the class Dog of testMod!!")
+        return "BBB"
+
 def cat():
     print("I am a cat,miao miao miao!! ")
