f004babb49a4b34094417b1a6d9575e9bfcd6270
kuman-lenovo <775121173@qq.com>
2017-10-22 11:38:22 +0800

    成功输出文件了，利用了os.getcmd(),而且，自己忘记了，如果python要打印的时候，变量需要统一成为字符串、

--- trySpider1.py
+++ trySpider1.py
@@ -7,9 +7,18 @@ import os
 ##定义好数量
 
 
-def pachong():
+def pachong(y):
     time.sleep(1)
-    print("hello world!!")
+
+    #fileName = "hello world!!"+str(y)
+    #print(fileName)
+    #print("F:\pythonTest\weixinOA\\"+str(y))
+    #f = open("F:\pythonTest\weixinOA\\"+'test'+str(y)+".txt",'w')
+    baseDir = os.getcwd()
+    f = open(baseDir+'\\test'+str(y)+".txt",'w')
+    f.write("hello world"+str(y))
+    f.close()
+    #如何将内容生成在本地文件呢？？问的好，现在就去看看
     #res = urllib.request.urlopen("www.baidu.com")
 
 ##程序必写的这句话
@@ -19,14 +28,14 @@ def main():
     urlSite = ["http://www.baidu.com","http://163.com","http://www.520it.com"]
 ##定义进程池
 ##定义好数量
-    #定义好进程池之后，下面就可以写个循环，把任务代入进程池进行。
+#定义好进程池之后，下面就可以写个循环，把任务代入进程池进行。
     pa = Pool(3)
     for x in range(3):
-        pa.apply_async(pachong)
+            pa.apply_async(pachong,args=(x,))
     pa.close()
     pa.join()
 
 if __name__ == "__main__":
     main()
-    print(os.getcwd())
+    #print(os.getcwd())
 
